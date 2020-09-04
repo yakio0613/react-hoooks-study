b2eb5e55b4fddf18099bd5c04abd5288db76c637
蓮沼愛雄 <hasunumayoshio@MacBook-Pro.local>
2020-09-05 05:02:12 +0900

    update hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+const App = props => {
 
-  const increment2 = () => setCount(previousCount  => previousCount + 1)
-  const decrement2 = () => setCount(previousCount  => previousCount - 1)
-  const double = () => setCount(previousCount  => previousCount * 2)
-  const waru = () => setCount(previousCount  => { 
-    if (previousCount % 3 === 0) {
-      return previousCount / 3
-    } else {
-      return previousCount
-    }
-  })
+  const [ name, setName ] = useState(props.name)
+  const [ price, setPrice ] = useState(props.price)
 
-  const reset = () => setCount(0)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+
   return (
     <>
-      <div>count: {count}</div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={increment2}>+1</button>
-      <button onClick={decrement2}>-1</button>
-      <button onClick={double}>x2</button>
-      <button onClick={waru}>/3</button>
-      <button onClick={reset}>reset</button>
+    <div>現在の{name}は{price}円です。</div>
+    <button onClick={() => setPrice(price + 1)}>+1</button>
+    <button onClick={() => setPrice(price - 1)}>-1</button>
+    <button onClick={reset}>reset</button>
+    <input value={name} onChange={e =>setName(e.target.value)} />
     </>
   )
+}
+
+App.defaultProps = {
+  name: "空",
+  price: 1000
 }
 
 export default App
