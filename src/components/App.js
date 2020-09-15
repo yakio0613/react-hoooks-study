47c3028b630124b6f24e563f881667c890e6246f
蓮沼愛雄 <hasunumayoshio@MacBook-Pro.local>
2020-09-16 05:45:18 +0900

    setup context

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import EventForm from './EventForm'
 import Events from './Events'
+import AppContext from '../context/AppContext'
 import reducer from '../reducers'
+
+console.log({AppContext})
 
 const App = () => {
   const [ state, dispatch ] = useReducer(reducer, [])
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value={'Hello provider'}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   )
 }
 
